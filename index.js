b3369b135f0265c0fbf39f719ccf381657f68ce9
jmoypace <jmoypace@gmail.com>
2018-05-17 14:50:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ function makeInt(n){
 }
 function makeInt(n){
   return parseInt(n,10)
+<<<<<<< HEAD
 }
 function preserveDecimal(n){
   return parseFloat(n)
+=======
+>>>>>>> 88ccd4069da1820c220b80e7a43bfca82921375f
 }
